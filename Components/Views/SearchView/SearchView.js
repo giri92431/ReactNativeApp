a2b845019e8eb11c964cdd62a40a38eb748c81a1
Euromonitor <euromonitor@Euromonitors-MacBook-Pro.local>
2017-12-05 14:16:42 +0530

    search tab completed

--- Components/Views/SearchView/SearchView.js
+++ Components/Views/SearchView/SearchView.js
@@ -18,15 +18,55 @@ import DataFactoryApi from './../../../Data/DataFactory'
     var homeData = new DataFactoryApi('search')
     return homeData.createData()
   }
+  styles = StyleSheet.create({
+		
+		title: {
+			fontSize: 24,
+			fontWeight: 'bold',
+			padding: 5,
+      color: '#436060',
+      textAlign: 'center',
+     
+		},
+		commentsTitle: {
+      fontSize:20,
+      color: '#587272',
+      textAlign:'center',
+      padding:5,textDecorationLine: 'underline',
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    video: {
+      marginTop: 20,
+      minHeight: 290,
+      width: 320,
+      flex: 1
+    },
+    image:{
+      width: 300, height: 300
+    },
+    Center:{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    }
 
+		
+	});
   _image(item){
     return(
-      item.map((img,j)=>
-      <Image key={j}
-      style={{width: 300, height: 250}}
-      source={{uri: img}}
-      />
-    )
+      <View style= {this.styles.Center}>
+      {
+        item.map((img,j)=>
+          <Image key={j}
+          style={this.styles.image}
+          source={{uri: img}}
+          />
+    )}
+    </View>
     )
   }
   _CommentAmazon(item){
@@ -35,7 +75,7 @@ import DataFactoryApi from './../../../Data/DataFactory'
       
       return(
         <View>
-        <Text> Review on amazon</Text>
+        <Text style={this.styles.commentsTitle}> Review on amazon</Text>
         {item.map((ama,k)=>
         <Text key={k}>{ama}</Text>
       )}
@@ -49,7 +89,7 @@ import DataFactoryApi from './../../../Data/DataFactory'
       
       return(
         <View>
-        <Text> Review on Twitter</Text>
+        <Text style={this.styles.commentsTitle}> Review on Twitter</Text>
         {item.map((twt,k)=>
         <Text key={k} >{twt}</Text>
       )}
@@ -64,7 +104,7 @@ import DataFactoryApi from './../../../Data/DataFactory'
       
       return(
         <View>
-        <Text> Review on facebook</Text>
+        <Text style={this.styles.commentsTitle}> Review on facebook</Text>
         {item.map((twt,k)=>
         <Text key={k}>{twt}</Text>
       )}
@@ -79,7 +119,7 @@ import DataFactoryApi from './../../../Data/DataFactory'
       
       return(
         <View>
-        <Text> Review on sanpdeal</Text>
+        <Text style={this.styles.commentsTitle}> Review on filpkart</Text>
         {item.map((twt,k)=>
         <Text key={k}>{twt}</Text>
       )}
@@ -87,20 +127,44 @@ import DataFactoryApi from './../../../Data/DataFactory'
       )
     }
   };
+  _webView(item){
+    if(item.length != 0)
+    {
+      
+    return(
+      <View>
+        <Text style={this.styles.commentsTitle}>Videos on youtube</Text>
+        {item.map((vid,m)=>
+         <View key ={m} style={this.styles.container}>
+          <WebView
+            source={{uri: vid}}
+            style={this.styles.video}
+            
+         />
+        </View>
+        )}
+       
+      </View>
+    )
+  }
+  }
 
  	render(){
  		return(
+
       <ScrollView >
       <View>
       {
         this.tmp_array.map((item, i) => (
           <View key={i}>
-            <Text>{item.name}</Text>
+          
+            <Text style={this.styles.title}>{item.name}</Text>
             <View>
               {
                 this._image(item.image)
               }
             </View>
+            <View style={this.styles.Center}>
             <Rating
                 showRating
                 type="star"
@@ -111,14 +175,16 @@ import DataFactoryApi from './../../../Data/DataFactory'
                 onFinishRating={this.ratingCompleted}
                 style={{ paddingVertical: 10 }}
               />
+              </View>
+              <View style={{margin:10}}>
               {this._CommentAmazon(item.comments.amazon)}
-              {this._CommentSnapDeal(item.comments.sanpdeal)}
+              {this._CommentSnapDeal(item.comments.filpkart)}
               {this._CommentTwitter(item.comments.twitter)}
               {this._CommentFacebook(item.comments.facebook)}
-              
-            
+             { this._webView(item.video)}
+             </View>
+          <View style={{height:400}}></View>
           </View>
-          
         )
       )
       }
@@ -127,5 +193,7 @@ import DataFactoryApi from './../../../Data/DataFactory'
    )
  					
  		
- 	}
+   }
+   
+
  }
